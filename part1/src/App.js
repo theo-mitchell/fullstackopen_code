be8a78475d0bfce42f1a396f3142cc04b89795ba
theom95 <theodore.mitchell95@gmail.com>
2022-10-22 16:13:24 -0500

    finished counter app

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,32 +1,16 @@
-const Hello = (props) => {
-  return (
-    <div>
-      {" "}
-      <p>Hello {props.name}, you are {props.age} years old</p>{" "}
-    </div>
-  );
-};
+import { useState } from 'react'
 
-const Footer = () => {
+const App = () => {
+  const [ counter, setCounter ] = useState(0)
+
   return (
     <div>
-      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+      <div>{counter}</div>
+      <button onClick={() => setCounter(counter + 1)}>Increase counter</button>
+      <button onClick={() => setCounter(0)}>Reset counter</button>
     </div>
+    
   )
 }
 
-const App = () => {
-  const name = 'Peter';
-  const age = 10;
-
-  return (
-    <>
-      <h1>Greetings</h1>
-      <Hello name='George' age={40 + 10}/>
-      <Hello name={name} age={age}/>
-      <Hello />
-      <Footer />
-    </>
-  );
-};
-export default App;
+export default App
