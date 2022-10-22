cd355a590d6e5332e564b05974bdb0a72fb40912
theom95 <theodore.mitchell95@gmail.com>
2022-10-22 17:02:54 -0500

    finished left/right click

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,24 +1,53 @@
-import { useState } from "react";
+import { useState } from "react"
 
-const Display = ({ counter }) => <div>{counter}</div>;
+const History = (props) => {
+  if (props.allClicks.length === 0) {
+    return (
+      <div>
+        The app is used by pressing the buttons
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        button press history: {props.allClicks.join(' ')}
+      </div>
+    )
+  };
+}
 
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
 
 const App = () => {
-  const [counter, setCounter] = useState(0);
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  const increaseByOne = () => setCounter(counter + 1);
-  const decreaseByOne = () => setCounter(counter - 1);
-  const resetToZero = () => setCounter(0);
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  debugger;
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text={"Add one"} />
-      <Button onClick={decreaseByOne} text={"Subtract one"} />
-      <Button onClick={resetToZero} text={"Reset to zero"} />
+      {left}
+      <Button handleClick={handleLeftClick} text={'left'}/>
+      <Button handleClick={handleRightClick} text={'right'}/>
+      {right}
+      <History allClicks={allClicks}/>
     </div>
-  );
-};
+  )
+}
 
 export default App;
