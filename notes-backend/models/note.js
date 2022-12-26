c9bb27aa77766085a17c530f4cd5621ab0cfa44c
theom95 <theodore.mitchell95@gmail.com>
2022-12-26 15:09:08 -0600

    fixed mongo auth

--- notes-backend/models/note.js
+++ notes-backend/models/note.js
@@ -1,29 +1,24 @@
-require("dotenv-flow").config();
-const mongoose = require("mongoose");
-
-const login = encodeURIComponent(process.env.MONGODB_LOGIN);
-const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
-const url = `mongodb+srv://${login}:${password}@cluster0.wvoz7g2.mongodb.net/noteApp?retryWrites=true&w=majority`;
-
-mongoose.connect(url).then((result) => {
-  console.log("---CONNECTED---");
-}).catch((error) => {
-    console.log('ERROR CONNECTING TO MONGODB:', error.message);
-});
+const mongoose = require('mongoose')
 
 const noteSchema = new mongoose.Schema({
-  content: String,
-  date: Date,
-  important: Boolean,
-});
-
-noteSchema.set("toJSON", {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-
-    delete returnedObject._id;
-    delete returnedObject.__v;
+  content: {
+    type: String,
+    required: true,
+    minlength: 5
   },
-});
+  date: {
+    type: Date,
+    required: true,
+  },
+  important: Boolean,
+})
 
-module.exports = mongoose.model("Note", noteSchema);
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
---
+++ notes-backend/utils/config.js
@@ -0,0 +1,19 @@
+require("dotenv-flow").config();
+
+const PORT = process.env.PORT;
+
+const login = encodeURIComponent(process.env.MONGODB_LOGIN);
+const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
+
+const URI=`mongodb+srv://${login}:${password}@cluster0.wvoz7g2.mongodb.net/noteApp?retryWrites=true&w=majority`
+const TEST_URI=`mongodb+srv://${login}:${password}@cluster0.wvoz7g2.mongodb.net/noteAppTestDB?retryWrites=true&w=majority`
+
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? TEST_URI
+    : URI;
+
+module.exports = {
+  MONGODB_URI,
+  PORT,
+};
