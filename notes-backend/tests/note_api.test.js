b0a53ac71a8db68ab19b959b1edd59e2553e4c1b
theom95 <theodore.mitchell95@gmail.com>
2022-12-26 17:49:13 -0600

    added async and new tests (backend)

--- notes-backend/tests/note_api.test.js
+++ notes-backend/tests/note_api.test.js
@@ -1,5 +1,55 @@
 const mongoose = require("mongoose");
-const supertest = require("supertest");
+const request = require("supertest");
 const app = require("../app");
 
-const api = supertest(app);
+const Note = require("../models/note");
+
+const initialNotes = [
+  {
+    content: "HTML is easy",
+    date: new Date(),
+    important: false,
+  },
+  {
+    content: "Browser can execute only Javascript",
+    date: new Date(),
+    important: true,
+  },
+];
+
+beforeEach(async () => {
+  await Note.deleteMany({});
+
+  let noteObject = new Note(initialNotes[0]);
+  await noteObject.save();
+  noteObject = new Note(initialNotes[1]);
+  await noteObject.save();
+});
+
+test("notes are returned as json", async () => {
+  await request(app)
+    .get("/api/notes")
+    .expect(200)
+    .expect("Content-Type", /application\/json/);
+}, 100000);
+
+test("all notes are returned", async () => {
+  const response = await request(app)
+    .get("/api/notes")
+    .catch((err) => console.log("HA", err));
+
+  expect(response.body).toHaveLength(initialNotes.length);
+}, 100000);
+
+test("a specific note is within the returned notes", async () => {
+  const response = await request(app).get("/api/notes");
+
+  const contents = response.body.map((r) => r.content);
+  expect(contents).toContain("Browser can execute only Javascript");
+
+  expect(response.body[0].content).toBe("HTML is easy");
+});
+
+afterAll(() => {
+  mongoose.connection.close();
+});
--- notes-backend/utils/config.js
+++ notes-backend/utils/config.js
@@ -5,13 +5,12 @@ const PORT = process.env.PORT;
 const login = encodeURIComponent(process.env.MONGODB_LOGIN);
 const password = encodeURIComponent(process.env.MONGODB_PASSWORD);
 
-const URI=`mongodb+srv://${login}:${password}@cluster0.wvoz7g2.mongodb.net/noteApp?retryWrites=true&w=majority`
-const TEST_URI=`mongodb+srv://${login}:${password}@cluster0.wvoz7g2.mongodb.net/noteAppTestDB?retryWrites=true&w=majority`
+// console.table({ login: login, password: password, port: PORT });
 
-const MONGODB_URI =
-  process.env.NODE_ENV === "test"
-    ? TEST_URI
-    : URI;
+const URI = `mongodb+srv://${login}:${password}@cluster0.wvoz7g2.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const TEST_URI = `mongodb+srv://${login}:${password}@cluster0.wvoz7g2.mongodb.net/noteAppTest?retryWrites=true&w=majority`;
+
+const MONGODB_URI = process.env.NODE_ENV === "test" ? TEST_URI : URI;
 
 module.exports = {
   MONGODB_URI,
