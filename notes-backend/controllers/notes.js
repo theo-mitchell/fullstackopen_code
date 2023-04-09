0b4fc117815878f0d5db0af0d99316315d0e53c7
theom95 <theodore.mitchell95@gmail.com>
2023-04-09 18:59:12 -0500

    feat: added token auth

--- notes-backend/controllers/notes.js
+++ notes-backend/controllers/notes.js
@@ -1,12 +1,23 @@
 const User = require("../models/user");
 const notesRouter = require("express").Router();
 const Note = require("../models/note");
+const jwt = require("jsonwebtoken");
 
 notesRouter.get("/", async (request, response) => {
   const notes = await Note.find({}).populate("user", { username: 1, name: 1 });
 
   response.json(notes);
 });
+
+const getTokenFrom = (request) => {
+  const authorization = request.get("authorization");
+
+  if (authorization && authorization.startsWith("Bearer ")) {
+    return authorization.replace("Bearer ", "");
+  }
+
+  return null;
+};
 
 notesRouter.get("/:id", async (request, response, next) => {
   const note = await Note.findById(request.params.id);
@@ -20,14 +31,19 @@ notesRouter.get("/:id", async (request, response, next) => {
 
 notesRouter.post("/", async (request, response, next) => {
   const body = request.body;
+  const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET);
 
-  const user = await User.findById(body.userId);
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: "invalid token" });
+  }
+
+  const user = await User.findById(decodedToken.id);
 
   const note = new Note({
     content: body.content,
-    important: body.important || false,
+    important: body.important === undefined ? false : body.important,
     date: new Date(),
-    user: user.id,
+    user: user._id,
   });
 
   const savedNote = await note.save();
